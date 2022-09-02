2e3a5e2406088a534af019ac067880321dba094b
mixice <mixice@mixice.com>
2022-09-02 15:53:09 +0800

    new

--- js/uigg.js
+++ js/uigg.js
@@ -761,6 +761,31 @@ $(function(){
     });
 });
 
+//----------------------------------------------------------------------------------cookie
+$(function(){
+    //input cookie
+    function setCookie(objName, objValue, objHours){
+        var str = objName + '=' + encodeURI(objValue);
+        if (objHours > 0) {
+            var date = new Date();
+            var ms = objHours * 3600 * 1000;
+            date.setTime(date.getTime() + ms);
+            str += '; expires=' + date.toGMTString();
+        };
+        document.cookie = str;
+    };
+
+    //output cookie
+    function getCookie(objName){
+        var arrStr = document.cookie.split('; ');
+        for (var i = 0; i < arrStr.length; i++){
+            var temp = arrStr[i].split('=');
+            if (temp[0] == objName) return decodeURI(temp[1]);
+        };
+        return '';
+    };
+});
+
 //----------------------------------------------------------------------------------
 $(function(){
 
